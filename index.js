382e418d4fb53d5c816c80f758a136d2172800e5
Anthony <anthonykimlao@gmail.com>
2023-12-26 09:22:43 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+  function superbowlWin(record){
+            for (const item of record){
+                if (item.result === "W"){
+                    return record.find(x=>x.result==="W").year;
+                }
+                else if (item.result ==! "W"){
+                return record.find(x=>x.result==="W");
+                }
+      }
+}
